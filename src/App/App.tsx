b1a1cc1ad50a7b1b8f54294540e97ca7e525960c
EliseyBelov <elisey.belov777@gmail.com>
2023-09-06 19:37:43 +0300

    init hw-3

---
+++ src/App/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+import './App.css';
+
+const App = () => {
+  return <div></div>;
+};
+
+export default App;
---
+++ src/pages/Products/Products.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import './Products.scss';
+
+export type ProductsProps = {}
+
+const Products: React.FC<ProductsProps> = ({}) => {
+  return <div></div>;
+};
+
+export default Products;
